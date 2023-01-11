719db5eeaf785dfc3a3cd1d899efa54357ed5333
LamaElkhouk <lamiaelkhoukhi09@gmail.com>
2023-01-11 19:03:02 +0100

    css mobile, menu burger et css cards vols

--- exercices/vue-typescript/skyscanner/src/components/Menu.vue
+++ exercices/vue-typescript/skyscanner/src/components/Menu.vue
@@ -2,20 +2,26 @@
     <!-- Sidebar-->
     <div class="menu container">
         <nav class="menu-nav">
-            <p class="title">
-                <RouterLink  :to="{name:'accueil'}"  class="navbar-brand">
-                            SkyScanner
+            <div class="title">
+                <RouterLink  :to="{name:'accueil'}"  class="menu-title">
+                    SkyScanner 
                 </RouterLink>
-            </p>
-            <ul>
+                <svg  @click.prevent="menu_burger" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-list afficher-sur-mobile" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </div>
+            <ul class="afficher-sur-pc menu-ul">
+                <svg @click.prevent="menu_close" xmlns="http://www.w3.org/2000/svg"  class="bi bi-plus-lg hidden" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                </svg>
                 <li>
                     <RouterLink  :to="{name:'accueil'}"  class="nav-link">
-                        Accueil
+                        <span class="dot-menu hidden"></span> Accueil
                     </RouterLink>
                 </li>
                 <li>
                     <RouterLink  :to="{name:'vols'}"  class="nav-link">
-                        vols
+                        <span class="dot-menu hidden"></span> Vols
                     </RouterLink>
 
                 </li>
@@ -36,6 +42,41 @@ export default defineComponent({
         RouterLink
     },
     methods:{
+        menu_burger(){
+            const menuUl= document.querySelector(".menu-ul") as HTMLElement
+
+            menuUl.classList.remove("afficher-sur-pc");
+            menuUl.classList.add("afficher-sur-mobile");
+
+            const dotMenu=document.querySelector(".dot-menu") as HTMLElement
+            dotMenu.classList.remove("hidden");
+
+            const cross=document.querySelector(".bi-plus-lg") as HTMLElement
+            cross.classList.remove("hidden");
+            //eviter le scroll
+
+            const html=document.querySelector("html");
+            html.classList.add("scroll-menu");
+        },
+        menu_close(){
+            const menuUl= document.querySelector(".menu-ul") as HTMLElement
+
+            menuUl.classList.remove("afficher-sur-mobile");
+            menuUl.classList.add("afficher-sur-pc");
+
+            const dotMenu=document.querySelector(".dot-menu") as HTMLElement
+            dotMenu.classList.add("hidden");
+
+            const cross=document.querySelector(".bi-plus-lg") as HTMLElement
+            cross.classList.add("hidden");
+
+            //scroll
+
+            const html=document.querySelector("html");
+            html.classList.remove("scroll-menu");
+
+        }
+
     }
 
 })
--- exercices/vue-typescript/skyscanner/src/components/Vols.vue
+++ exercices/vue-typescript/skyscanner/src/components/Vols.vue
@@ -34,36 +34,138 @@
             <div class="container-spinner">
                 <div class="spinner-border text-info" role="status" v-if='isLoading && !vols.length'></div>
             </div>
-            
             <div class="cards-vols container">
+                <!--test visuel avec donnée en dure -->
+                <div class="card-vol" >
+                        <div class="informations-vol">
+                            <div class="information-vol-aller">
+                                <svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                </svg>
+                                <div class="infos-titre-aller">
+                                    <h4>aller</h4>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-airplane-fill" viewBox="0 0 16 16">
+                                        <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Z"/>
+                                    </svg>
+                                </div>
+                                
+                                <p class="infos">Aller - Retour: <strong>LON</strong>, london - <strong>NYCA </strong>, new york </p>
+                                <p class="infos"><span class="dot"></span> Depart : Le 11/01/2023 à 10h00</p>
+                                <p class="infos"><span class="dot"></span> Arrivée :Le 11/01/2023 à 15h00</p>
+                                <p class="infos"><span class="dot"></span> Durée 6h00</p>
+                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-arrow-down-up" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
+                            </svg>
+                            <div class="information-vol-retour">
+                                <div class="infos-titre-retour">
+                                    <h4>retour</h4>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-airplane-fill" viewBox="0 0 16 16">
+                                        <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Z"/>
+                                    </svg>
+                                </div>
+                                
+                                <p class="infos">Aller - Retour: <strong>NYAC</strong>, new york - <strong>LON </strong>, london</p>
+                                <p class="infos"><span class="dot"></span> Départ : Le 20/01/2023 à 06h00</p>
+                                <p class="infos"><span class="dot"></span> Arrivée :Le 20/01/2023 à 11h00</p>
+                                <p class="infos"><span class="dot"></span> Durée 08h00</p>
+                            </div>
+                        </div>
+                        <div  class="infos-en-savoir-plus">
+                            <h3>Prix totale: 100€</h3>
+                            <RouterLink :to="{name:'accueil',params:{}}" class="btn btn-primary">En savoir plus</RouterLink>
+                        </div>
+                        
+                </div>
+                <!--aller uniquement-->
+                <div class="card-vol" >
+                        <div class="informations-vol">
+                            <div class="information-vol-aller">
+                                <svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                </svg>
+                                <div class="infos-titre-aller">
+                                    <h4>aller</h4>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-airplane-fill" viewBox="0 0 16 16">
+                                        <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Z"/>
+                                    </svg>
+                                </div>
+                                
+                                <p class="infos">Aller - Retour: <strong>LON</strong>, london - <strong>NYCA </strong>, new york </p>
+                                <p class="infos"><span class="dot"></span> Depart : Le 11/01/2023 à 10h00</p>
+                                <p class="infos"><span class="dot"></span> Arrivée :Le 11/01/2023 à 15h00</p>
+                                <p class="infos"><span class="dot"></span> Durée 6h00</p>
+                            </div>
+                        </div>
+                    <div  class="infos-en-savoir-plus">
+                        <h3>Prix totale: 100€</h3>
+                        <RouterLink :to="{name:'accueil',params:{}}" class="btn btn-primary">En savoir plus</RouterLink>
+                    </div>
+                </div>
+                <!--fin du teste-->
+
+                <!--données issu de l'api-->
                 <div v-for="vol in vols " :key="vol.id">
+                    <!-- aller - retour-->
                     <div class="card-vol" v-if="returnDate!=''" >
                         <div class="informations-vol">
                             <div class="information-vol-aller">
-                                <h3>aller</h3>
-                                <p style="color:red">aller - retour: <strong>{{ vol.legs[0].origin.display_code}}</strong>, {{ vol.legs[0].origin.name }} - <strong>{{ vol.legs[0].destination.display_code }} </strong>, {{ vol.legs[0].destination.name }} </p>
-                                <p> depart : Le {{ convertir_date(vol.legs[0].departure)}} à {{convertir_heure(vol.legs[0].departure)}}</p>
-                                <p> arrivée :Le {{ convertir_date(vol.legs[0].arrival)}} à {{ convertir_heure(vol.legs[0].arrival)}}</p>
-                                <p>duree {{convertir_duree(vol.legs[0].duration)}}</p>
+                                <svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                </svg>
+                                <div class="infos-titre-aller">
+                                    <h4>Aller</h4>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-airplane-fill" viewBox="0 0 16 16">
+                                        <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Z"/>
+                                    </svg>
+                                </div>
+                                <p>Aller - Retour: <strong>{{ vol.legs[0].origin.display_code}}</strong>, {{ vol.legs[0].origin.name }} - <strong>{{ vol.legs[0].destination.display_code }} </strong>, {{ vol.legs[0].destination.name }} </p>
+                                <p class="infos" >Départ : Le {{ convertir_date(vol.legs[0].departure)}} à {{convertir_heure(vol.legs[0].departure)}}</p>
+                                <p class="infos">Arrivée :Le {{ convertir_date(vol.legs[0].arrival)}} à {{ convertir_heure(vol.legs[0].arrival)}}</p>
+                                <p class="infos">Durée {{convertir_duree(vol.legs[0].duration)}}</p>
                             </div>
                             <div class="information-vol-retour">
-                                <h3>retour</h3>
-                                <p style="color:red">aller - retour: <strong>{{ vol.legs[1].origin.display_code}}</strong>, {{ vol.legs[1].origin.name }} - <strong>{{ vol.legs[1].destination.display_code }} </strong>, {{ vol.legs[1].destination.name }} </p>
-                                <p> depart : Le {{ convertir_date(vol.legs[1].departure)}} à {{convertir_heure(vol.legs[1].departure)}}</p>
-                                <p> arrivée :Le {{ convertir_date(vol.legs[1].arrival)}} à {{ convertir_heure(vol.legs[1].arrival)}}</p>
-                                <p>duree {{convertir_duree(vol.legs[0].duration)}}</p>
+                                <div class="infos-titre-retour">
+                                    <h4>Retour</h4>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-airplane-fill" viewBox="0 0 16 16">
+                                        <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Z"/>
+                                    </svg>
+                                </div>
+                                <p>Aller - Retour: <strong>{{ vol.legs[1].origin.display_code}}</strong>, {{ vol.legs[1].origin.name }} - <strong>{{ vol.legs[1].destination.display_code }} </strong>, {{ vol.legs[1].destination.name }} </p>
+                                <p class="infos"><span class="dot"></span> Départ : Le {{ convertir_date(vol.legs[1].departure)}} à {{convertir_heure(vol.legs[1].departure)}}</p>
+                                <p class="infos"><span class="dot"></span> Arrivée :Le {{ convertir_date(vol.legs[1].arrival)}} à {{ convertir_heure(vol.legs[1].arrival)}}</p>
+                                <p class="infos"><span class="dot"></span> Durée {{convertir_duree(vol.legs[0].duration)}}</p>
                             </div>
                         </div>
-                        <RouterLink :to="{name:'unVol', params:{vol: JSON.stringify(vol)}}" class="btn btn-primary">En savoir plus</RouterLink>
-                        <h2 style="color:grey">prix totale: {{vol.price.amount}}€</h2>
+                        <div  class="infos-en-savoir-plus">
+                            <h3>prix totale: {{vol.price.amount}}€</h3>
+                            <RouterLink :to="{name:'unVol', params:{vol: JSON.stringify(vol)}}" class="btn btn-primary">En savoir plus</RouterLink>
+                        </div>
                     </div>
-                    <div v-else style="information-vol-aller">
-                        <h3>aller</h3>
-                        <p style="color:red">aller - retour: <strong>{{ vol.legs[0].origin.display_code}}</strong>, {{ vol.legs[0].origin.name }} - <strong>{{ vol.legs[0].destination.display_code }} </strong>, {{ vol.legs[0].destination.name }} </p>
-                        <p> depart : Le {{ convertir_date(vol.legs[0].departure)}} à {{convertir_heure(vol.legs[0].departure)}}</p>
-                        <p> arrivée :Le {{ convertir_date(vol.legs[0].arrival)}} à {{ convertir_heure(vol.legs[0].arrival)}}</p>
-                        <p>duree {{convertir_duree(vol.legs[0].duration)}}</p>
-                        <h2 style="color:grey">prix totale: {{vol.price.amount}}€</h2>
+                        <!-- aller uniquement-->
+
+                    <div class="card-vol" v-else>
+                        <div class="informations-vol">
+                            <div class="information-vol-aller">
+                                <svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                </svg>
+                                <div class="infos-titre-aller">
+                                    <h4>Aller</h4>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-airplane-fill" viewBox="0 0 16 16">
+                                        <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Z"/>
+                                    </svg>
+                                </div>
+                                <p>Aller - Retour: <strong>{{ vol.legs[0].origin.display_code}}</strong>, {{ vol.legs[0].origin.name }} - <strong>{{ vol.legs[0].destination.display_code }} </strong>, {{ vol.legs[0].destination.name }} </p>
+                                <p class="infos" >Départ : Le {{ convertir_date(vol.legs[0].departure)}} à {{convertir_heure(vol.legs[0].departure)}}</p>
+                                <p class="infos">Arrivée :Le {{ convertir_date(vol.legs[0].arrival)}} à {{ convertir_heure(vol.legs[0].arrival)}}</p>
+                                <p class="infos">Durée {{convertir_duree(vol.legs[0].duration)}}</p>
+                            </div>
+                        </div>
+                        <div  class="infos-en-savoir-plus">
+                            <h3>prix totale: {{vol.price.amount}}€</h3>
+                            <RouterLink :to="{name:'unVol', params:{vol: JSON.stringify(vol)}}" class="btn btn-primary">En savoir plus</RouterLink>
+                        </div>
                     </div>
                 </div>
             </div>
